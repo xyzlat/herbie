392985c78dac3b95eddf791babacd5cb567f60e9
tobias.rosenstock <tobi.rosenstock@gmail.com>
2019-11-27 16:00:02 +0100

    refactor conroller test

--- wayneapp/services.py
+++ wayneapp/services.py
@@ -2,26 +2,34 @@ import importlib
 
 from wayneapp.models.models import AbstractBusinessEntity
 
+class BusinessEntityManager:
+    def get_business_entity_class(self, entity_name: str):
+        models_module = importlib.import_module("wayneapp.models")
 
-def get_business_entity_class(entity_name: str):
-    models_module = importlib.import_module("wayneapp.models")
-    return getattr(models_module, str(entity_name))
-
-
-def update_or_create_business_entity(entity_name: str, key: str, version: int, data: str) -> (AbstractBusinessEntity, bool):
-    business_entity_class = get_business_entity_class(entity_name)
-    obj, created = business_entity_class.objects.update_or_create(
-        key=key,
-        version=version,
-        defaults={
-            'key': key,
-            'version': version,
-            'data': data
-        }
-    )
-    return obj
+        return getattr(models_module, str(entity_name).capitalize())
 
 
-def delete_business_entity(entity_name: str, key: str, version: int) -> None:
-    business_entity_class = get_business_entity_class(entity_name)
-    business_entity_class.objects.filter(key=key, version=version).delete()
+    def update_or_create_business_entity(
+            self,
+            entity_name: str,
+            key: str,
+            version: int,
+            data: str
+    ) -> (AbstractBusinessEntity, bool):
+        business_entity = self.get_business_entity_class(entity_name)
+        object, created = business_entity.objects.update_or_create(
+            key=key,
+            version=version,
+            defaults={
+                'key': key,
+                'version': version,
+                'data': data
+            }
+        )
+
+        return object
+
+
+    def delete_business_entity(self, entity_name: str, key: str, version: int) -> None:
+        business_entity = self.get_business_entity_class(entity_name)
+        business_entity.objects.filter(key=key, version=version).delete()
