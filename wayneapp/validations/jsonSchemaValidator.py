2568c84bdaaccbfa715745aee59884a1032d7cc2
Christos Athinaiou <christos.athinaiou@project-a.com>
2019-12-03 15:00:29 +0100

    refactoring validator

--- wayneapp/validations/jsonSchemaValidator.py
+++ wayneapp/validations/jsonSchemaValidator.py
@@ -11,37 +11,27 @@ class JsonSchemaValidator:
         super().__init__(**kwargs)
         self._schema_loader = SchemaLoader()
 
-    def validate_schema(self, jsonData: json, type: str, version: str) -> json:
+    def validate_schema(self, json_data: json, type: str, version: str) -> json:
         schema = self._get_json_schema(type, version)
-        response = {StatusConstants.STATUS: StatusConstants.STATUS_OK}
-
         data_validated = Draft7Validator(schema)
-        iterator = 0
-
-        sorted_errors = sorted(data_validated.iter_errors(jsonData), key=lambda e: e.path)
-
+        sorted_errors = sorted(data_validated.iter_errors(json_data), key=lambda e: e.path)
+        errors = {}
         for error in sorted_errors:
             if error.validator == ResponseConstants.REQUIRED_KEY:
                 error_property = re.search("'(.+?)'", error.message)
                 if error_property:
-                    response[error_property.group(1)] = {
+                    errors[error_property.group(1)] = {
                         ResponseConstants.ERROR_MESSAGE: error.message,
                         ResponseConstants.VALIDATE_KEY: error.validator
                     }
             else:
                 for error_property in error.path:
-                    response[error_property] = {
+                    errors[error_property] = {
                         ResponseConstants.ERROR_MESSAGE: error.message,
                         ResponseConstants.VALIDATE_KEY: error.validator_value
                     }
-            iterator += 1
-
-        if iterator > 0:
-            response[StatusConstants.STATUS] = StatusConstants.STATUS_ERROR
-
-        return {ResponseConstants.RESPONSE_KEY: response}
+        return errors
 
     def _get_json_schema(self, type, version) -> json:
         schema = json.loads(self._schema_loader.load(type, version))
-
         return schema
