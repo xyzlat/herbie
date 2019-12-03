3cc95c4b715151c2bd176ba98f73a0c936c91719
Christos Athinaiou <christos.athinaiou@project-a.com>
2019-12-03 15:20:19 +0100

    service refactor schemaLoader load method

--- wayneapp/services.py
+++ wayneapp/services.py
@@ -52,12 +52,11 @@ class BusinessEntityManager:
 class SchemaLoader:
 
     def load(self, type: str, version: str) -> str:
-        file_content = pkgutil.get_data('wayne_json_schema', type + '/' + type + '_' + version + '.json')
-        if file_content == None:
-            json_string = '{}'
-        else:
-            json_string = file_content.decode('utf-8')
-
+        try:
+            file_content = pkgutil.get_data('wayne_json_schema', type + '/' + type + '_' + version + '.json')
+        except Exception:
+            return None
+        json_string = file_content.decode('utf-8')
         return json_string
 
     def get_all_business_entity_names(self):
