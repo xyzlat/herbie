76434636accde47d0838d7d241de8147f404f4d5
tobias.rosenstock <tobi.rosenstock@gmail.com>
2019-12-11 12:20:39 +0100

    fix tests

--- wayneapp/tests/test_controller.py
+++ wayneapp/tests/test_controller.py
@@ -4,20 +4,19 @@ from unittest.mock import MagicMock
 from rest_framework import status
 from rest_framework.test import APIClient
 
-from wayneapp.services import BusinessEntityManager, SchemaLoader, settings
+from wayneapp.services import BusinessEntityManager, settings
 
 
 class TestBusinessEntityController(TestCase):
-    def setUp(self):
-        settings.SCHEMA_PACKAGE_NAME = 'wayneapp.tests.test_schema'
 
     @classmethod
     def setUpClass(cls):
         super(TestBusinessEntityController, cls).setUpClass()
+        settings.SCHEMA_PACKAGE_NAME = 'wayneapp.tests.test_schema'
+
 
     @patch.object(BusinessEntityManager, 'update_or_create', return_value={MagicMock(), True})
     def test_create_business_entity_should_work(self, mock_manager):
-
         data = {
             'version': 'v1',
             'key': 'x-id',
@@ -26,6 +25,7 @@ class TestBusinessEntityController(TestCase):
                 'name': 'chris'
             }
         }
+
         client = APIClient()
         response = client.post('/api/test_entity/save', data, format='json')
 
@@ -35,28 +35,41 @@ class TestBusinessEntityController(TestCase):
     @patch.object(BusinessEntityManager, 'update_or_create', side_effect=Exception('Test'))
     def test_create_business_entity_should_fail(self, mock_manager):
         data = {
+            'version': 'v1',
+            'key': 'x-id',
             'payload': {
-                'version': 5,
-                'id': 1,
-                'fname': 'chris'
+                'testId': 132,
+                'name': 'chris'
             }
         }
 
-        client = APIClient()
-        response = client.post('/api/test/1', data, format='json')
+        with self.assertRaises(Exception):
+            client = APIClient()
+            response = client.post('/api/test_entity/save', data, format='json')
 
-        self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
+            self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-    @patch.object(BusinessEntityManager, 'delete_by_key', return_value={MagicMock(), True})
+    @patch.object(BusinessEntityManager, 'delete', return_value=1)
     def test_delete_business_entity_should_work(self, mock_manager):
+        data = {
+            'version': 'v1',
+            'key': 'x-id',
+        }
+
         client = APIClient()
-        response = client.delete('/api/test/1', format='none')
+        response = client.post('/api/test_entity/delete', data, format='json')
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    @patch.object(BusinessEntityManager, 'delete_by_key', side_effect=Exception('Test'))
+    @patch.object(BusinessEntityManager, 'delete', side_effect=Exception('Test'))
     def test_delete_business_entity_should_fail(self, mock_manager):
-        client = APIClient()
-        response = client.delete('/api/test/1', format='none')
+        data = {
+            'version': 'v1',
+            'key': 'x-id',
+        }
 
-        self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
+        with self.assertRaises(Exception):
+            client = APIClient()
+            response = client.post('/api/test_entity/delete', data, format='json')
+
+            self.assertEqual(response.status_code, status.HTTP_500_INTERNAL_SERVER_ERROR)
