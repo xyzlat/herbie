5c03a46814ff67610fd69f89509c3a542c7ed2b4
Christos Athinaiou <christos.athinaiou@project-a.com>
2019-12-18 17:20:22 +0100

    refactor

--- wayneapp/authentication/social_authentication.py
+++ wayneapp/authentication/social_authentication.py
@@ -5,10 +5,8 @@ from wayneapp.services.permission_manager import PermissionManager
 
 
 def process_roles(details, user, **kwargs):
-    schema_loader = SchemaLoader()
-    permission_manager = PermissionManager()
     if not User.objects.filter(username=user.get_username).exists():
         my_group = Group.objects.get(name='business_entities_view_permission')
         user.is_staff = True
-        my_group.user_set.add(user)
+        user.groups.add(my_group)
         user.save()
