2d80dba384217a4c86e40f72f72344c52126ab37
Christos Athinaiou <christos.athinaiou@project-a.com>
2019-12-18 16:30:47 +0100

    assign business entities view permission-group to social users

--- wayneapp/authentication/social_authentication.py
+++ wayneapp/authentication/social_authentication.py
@@ -1,4 +1,4 @@
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 
 from wayneapp.services import SchemaLoader
 from wayneapp.services.permission_manager import PermissionManager
@@ -8,9 +8,7 @@ def process_roles(details, user, **kwargs):
     schema_loader = SchemaLoader()
     permission_manager = PermissionManager()
     if not User.objects.filter(username=user.get_username).exists():
-        business_entities = schema_loader.get_all_business_entity_names()
-        for business_entity in business_entities:
-            permission = permission_manager.get_view_permission(business_entity)
-            user.user_permissions.add(permission)
+        my_group = Group.objects.get(name='business_entities_view_permission')
         user.is_staff = True
+        my_group.user_set.add(user)
         user.save()
