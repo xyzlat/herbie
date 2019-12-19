b194bcf6a435df37a66d7a40b7f7c26f45e218d7
Christos Athinaiou <christos.athinaiou@project-a.com>
2019-12-19 09:04:11 +0100

    rename to group

--- wayneapp/authentication/social_authentication.py
+++ wayneapp/authentication/social_authentication.py
@@ -6,7 +6,7 @@ from wayneapp.services.permission_manager import PermissionManager
 
 def process_roles(details, user, **kwargs):
     if not User.objects.filter(username=user.get_username).exists():
-        my_group = Group.objects.get(name='business_entities_view_permission')
+        my_group = Group.objects.get(name='business_entities_view_group')
         user.is_staff = True
         user.groups.add(my_group)
         user.save()
